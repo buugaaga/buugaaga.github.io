f8792852c46fff1738dddaf9cdcfeda6a1f97a28
Eldar Nasyrov <buugaaga@bk.ru>
2019-11-19 14:19:40 +0500

    Updates

---
+++ precache-manifest.ebe2f309b792bc9085b790e763658761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd57d8da2d071dfd40812f5cfcb4879b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64717cd0eb3874aaf690",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1406dbb841a6d81cc2be",
+    "url": "/static/js/2.b2f2b748.chunk.js"
+  },
+  {
+    "revision": "64717cd0eb3874aaf690",
+    "url": "/static/js/main.e59c6305.chunk.js"
+  },
+  {
+    "revision": "d40e5e8946f1df63c3f0",
+    "url": "/static/js/runtime-main.8e7d88e7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
