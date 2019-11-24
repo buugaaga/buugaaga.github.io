029b989b9c97eda12ce55b12ec645818f42aa869
Ildar <buugaaga@bk.ru>
2019-11-24 10:29:14 +0500

    little change

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import { NavBar } from '../NavBar';
 import { NavState } from '../../context/navState';
