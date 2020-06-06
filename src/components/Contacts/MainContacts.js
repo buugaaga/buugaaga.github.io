1c26107690d82e45f02df4c3d3dcba0902467385
Eldar Nasyrov <ildar.nasrov@gmail.com>
2020-06-06 13:08:49 +0500

    Add changes to resume

--- src/components/Contacts/MainContacts.js
+++ src/components/Contacts/MainContacts.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Box from '@material-ui/core/Box'
-import { makeStyles } from '@material-ui/styles'
+
 import {
   ListItem,
   List,
@@ -10,42 +10,35 @@ import {
   Link,
 } from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginTop: 10,
-  },
-}))
-
 export const MainContacts = ({ text }) => {
-  const classes = useStyles()
-
   return (
     <Box mt={10} mx={5}>
       <List>
         <ListSubheader>
-          <Typography variant='h3'>{text.title}</Typography>
+          <Typography variant="h3">{text.title}</Typography>
         </ListSubheader>
         <ListItem>
           {text.email}
-          <Link href='mailto:ildar.nasrov@gmail.com'>
+          <Link href="mailto:ildar.nasrov@gmail.com">
             &nbsp;ildar.nasrov@gmail.com
           </Link>
         </ListItem>
         <ListItem>
           {text.tel}
-          <Link href='tel:+79923437503'>&nbsp;+79923437503</Link>
+          <Link href="tel:+79923437503">&nbsp;+79923437503</Link>
         </ListItem>
         <ListItem>
           {text.social}
           <Link
-            href='https://www.linkedin.com/in/eldar-nasyrov-a0391b177/'
-            target='_blank'>
+            href="https://www.linkedin.com/in/eldar-nasyrov-a0391b177/"
+            target="_blank"
+          >
             &nbsp;Linkedin,
           </Link>
-          <Link href='https://twitter.com/ildarDanilovich' target='_blank'>
+          <Link href="https://twitter.com/ildarDanilovich" target="_blank">
             &nbsp;twitter,{' '}
           </Link>
-          <Link href='https://github.com/buugaaga' target='_blank'>
+          <Link href="https://github.com/buugaaga" target="_blank">
             &nbsp;github
           </Link>
         </ListItem>
